00889ff21eb4ae6fd9bde6dc64c57f8b78cdf602
Arjun101105 <91782844+Arjun101105@users.noreply.github.com>
2024-05-31 16:56:07 +0000

    container config done

---
+++ 01_basics/variables1.js
@@ -0,0 +1,18 @@
+const accountId = 101105
+let accountEmail = "arjunnnn@gmail.com"
+var accountPass = "696969"
+accountCity = "Vasai"
+let accountState;
+
+
+// console.log(accountId); this will throw an error as accountID is constant and cannot be changed
+
+accountEmail = "arjunOP@google.com"
+
+accountPass = "121212"
+accountCity = "Pune"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
+
