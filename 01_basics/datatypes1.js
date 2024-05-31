4bc3dae605ef1f777706cf7e508bc155eb8ca5f7
Arjun101105 <91782844+Arjun101105@users.noreply.github.com>
2024-05-31 17:28:13 +0000

    data types

--- 01_basics/datatypes1.js
+++ 01_basics/datatypes1.js
@@ -2,3 +2,18 @@ console.log(3+3);
 
 console.log("Arjun")
 
+let name = "Arjun" // string data type
+let age = 18 // number 
+let isLoggedIn = true // boolean
+
+// null => standalone value 
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Arjun")
+console.log(typeof null)
+console.log(typeof undefined)
+
