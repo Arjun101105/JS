24a7f7a77086fd208ebb9719da551482ae0caecd
Arjun101105 <91782844+Arjun101105@users.noreply.github.com>
2024-06-02 23:30:49 +0000

    comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,14 @@
+console.log(2>1);
+console.log(2>=1);
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log("COMPARING null and zero");
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
+
+ // strict check => checks fo rdatatype also
+ console.log("Strict Check");
+console.log("2" === 2);
+console.log("2" === "2");
