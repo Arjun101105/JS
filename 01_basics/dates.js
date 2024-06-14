c3f02e86d45699ffb06a5a242e51078e6da3413d
Arjun101105 <91782844+Arjun101105@users.noreply.github.com>
2024-06-15 02:29:57 +0530

    dates and nums

---
+++ 01_basics/dates.js
@@ -0,0 +1,27 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+
+// let newDate = new Date(2024, 5, 11)
+
+// let newDate = new Date(2024, 5, 11, 12, 5, 30)
+let newDate = new Date("06-10-2024") // mmddyy format
+
+console.log(newDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(newDate.getTime());
+
+let secDate = new Date()
+console.log(secDate);
+console.log(secDate.getDay()); // output shows 1 => which means MONDAY
+console.log(secDate.getMonth());
+
+newDate.toLocaleString('default', {
+    weekday: "long"
+})
---
+++ 01_basics/numsAndMath.js
@@ -0,0 +1,39 @@
+// +++++++++++++++++++++++++ NUMBERS ++++++++++++++++++++++ 
+const score = 100
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const newNum = 23.6969
+// console.log(newNum.toPrecision(3));
+
+const secNum = 123.6969
+// console.log(secNum.toPrecision(3));
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString());
+
+// ++++++++++++++++++++++ MATHS ++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4)); // gives absolute value
+
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+
+console.log(Math.min(4, 3, 6, 7 ,1)); // gives min value in an array ... similarly with math.max 
+
+console.log(Math.random()); // always returns random value btwn 0 and 1
+
+console.log((Math.random()*10) + 1); // now it returns a value btwn 0 and 10
+console.log(Math.floor(Math.random()*10) + 1); // wrapped in math.floor to return lowest number
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
