8e84ca4ef89b86ca6acd1d53d32b1ffd5639a87c
Arjun Nair <arjunnair101105@gmail.com>
2024-07-13 12:16:13 +0530

    call and this in JS

--- 10_OOP/call.js
+++ 10_OOP/call.js
@@ -1,13 +1,16 @@
 function SetUsername (username){
         // complex DB call
         this.username = username
+        console.log("Called");
 }
 
 function createUser(username, email, password){
-    SetUsername(username)
+    SetUsername.call(this, username)
     this.email = email
     this.password = password
 }
 
-const chai = new createUser("CHAI" , "chai@chaicode.com", 696969)
+const chai = new createUser("CHAI" , "chai@chaicode.com", "696969")
 console.log(chai);
+// git config --global user.email "you@example.com"
+// git config --global user.name "Your Name"
